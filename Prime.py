5bfa9924a280c191fc1fe3abb8873535671aabb4
irenejenifer <52645515+irenejenifer@users.noreply.github.com>
2019-07-09 23:52:56 +0530

    Create Prime.py

---
+++ Prime.py
@@ -0,0 +1,13 @@
+def isprime(n):
+    if(n<2):
+        return False
+    for i in range(2,n//2):
+        if(n%i==0):
+            return False
+        return True
+
+num=int(input())
+if isprime(num):
+    print("yes")
+else:
+    print("no")
