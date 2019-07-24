8007504fdd8ec7a7c98c3966f04405db1e1b98fb
irenejenifer <52645515+irenejenifer@users.noreply.github.com>
2019-07-24 12:14:35 +0530

    Update listnew.py

--- listnew.py
+++ listnew.py
@@ -1,23 +1,22 @@
-x=int(input())
-y=input()
-y=list(map(int,y.split()))
+a=int(input())
+b=list(map(int,input().split()))
 o=[]
 sf=0
-for i in range (0,x):
+for i in range (0,a):
 	count=1
-	if(y[i]>0):
+	if(b[i]>0):
 		sf=1
 	else:
 		sf=0
-	for j in range (i+1,x):
+	for j in range (i+1,a):
 		if(sf==1):
-			if(y[j]<0):
+			if(b[j]<0):
 				sf=0
 				count+=1
 			else:
 				break
 		else:
-			if(y[j]>0):
+			if(b[j]>0):
 				sf=1
 				count+=1
 			else:
